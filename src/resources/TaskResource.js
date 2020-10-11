4d4905f2b3ce97d1f7749829240306b8c040f417
relaxgo <patientzhe@gmail.com>
2020-10-11 20:02:53 +0800

    add task resource

---
+++ src/resources/TaskResource.js
@@ -0,0 +1,17 @@
+import Resource from '../resource.js'
+
+let taskId = 1
+export default class TaskResource extends Resource {
+  type = 'Task'
+  constructor({ chunk = 1, task }) {
+    super()
+    this.name = 'task' + taskId++
+    this.chunk = chunk
+    this.task = task
+  }
+
+  async request(ctx, next) {
+    await this.task()
+    return next()
+  }
+}
